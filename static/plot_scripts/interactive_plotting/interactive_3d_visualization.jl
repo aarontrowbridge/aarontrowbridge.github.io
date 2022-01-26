f7232aa1cb08cbb4c39349e7a9e9f9cbf898ceca
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2022-01-26 13:57:44 -0500

    updating plotting script: merged html file gen

--- static/plot_scripts/interactive_plotting/interactive_3d_visualization.jl
+++ static/plot_scripts/interactive_plotting/interactive_3d_visualization.jl
@@ -1,5 +1,20 @@
-using WGLMakie
+using WGLMakie; WGLMakie.activate!()
 using JSServe
+
+n = 10
+volume = rand(n, n, n)
+fig, ax, plotobj = contour(volume; figure=(resolution=(700, 700),))
+
+output_file = "static/plot_html/interactive_plotting/contour.html"
+open(output_file, "w") do io
+    println(io, "<center>")
+    show(io, MIME"text/html"(), Page(exportable=true, offline=true))
+    show(io, MIME"text/html"(), fig)
+    println(io, "</center>")
+end
+
+
+
 
 # this is optional and just changes the color theme
 set_theme!(theme_dark())
@@ -13,14 +28,14 @@ ys = LinRange(-5, 5, 150)
 
 # creating the javascript app
 app = App() do session::Session
-    scale_slider = Slider(1:0.2:3)
+    scale_slider = Slider(1:3)
 
     states = map(scale_slider) do a
         return [radial_sinc(x, y, a) for x in xs, y in ys]
     end
 
     fig, ax, = surface(xs, ys, states, figure=(resolution=(700, 400),))
-    limits!(ax, -5, 5, -5, 5, -1, 2)
+    # limits!(ax, -5, 5, -5, 5, -1, 2)
 
     scale_value = DOM.div("\\(a = \\)", scale_slider.value)
     
@@ -33,9 +48,7 @@ end
 output_file = "static/plot_html/interactive_plotting/sinc_surface.html"
 open(output_file, "w") do io
     println(io, "<center>")
-    show(io, MIME"text/html"(), Page(exportable=true, offline=true))
+    # show(io, MIME"text/html"(), Page(exportable=true, offline=true))
     show(io, MIME"text/html"(), app)
     println(io, "</center>")
 end
-
-
